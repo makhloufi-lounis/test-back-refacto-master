1eb2eb30a06cbbf4e6d99633d7eaf6b1ed489613
Lounis Makhloufi <makhloufi.lounis@gmail.com>
2020-05-28 01:37:16 +0200

    update entity Template

--- src/Entity/Template.php
+++ src/Entity/Template.php
@@ -2,14 +2,78 @@
 
 class Template
 {
-    public $id;
-    public $subject;
-    public $content;
 
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $subject;
+
+    /**
+     * @var string
+     */
+    private $content;
+
+    /**
+     * Template constructor.
+     * @param $id
+     * @param $subject
+     * @param $content
+     */
     public function __construct($id, $subject, $content)
     {
         $this->id = $id;
         $this->subject = $subject;
         $this->content = $content;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param mixed $subject
+     * @return Template
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     * @return Template
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+        return $this;
+    }
+
+
 }
