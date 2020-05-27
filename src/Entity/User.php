fdae68fb14af04c827c7e44e2fecf3de5462b0b8
Lounis Makhloufi <makhloufi.lounis@gmail.com>
2020-05-28 01:53:06 +0200

    update entity User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,17 +1,105 @@
 <?php
 
+/**
+ * Class User
+ */
 class User
 {
-    public $id;
-    public $firstname;
-    public $lastname;
-    public $email;
+    /**
+     * @var int
+     */
+    private $id;
 
-    public function __construct($id, $firstname, $lastname, $email)
+    /**
+     * @var string
+     */
+    private $first_name;
+
+    /**
+     * @var string
+     */
+    private $last_name;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * User constructor.
+     * @param $id
+     * @param $first_name
+     * @param $last_name
+     * @param $email
+     */
+    public function __construct($id, $first_name, $last_name, $email)
     {
         $this->id = $id;
-        $this->firstname = $firstname;
-        $this->lastname = $lastname;
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
         $this->email = $email;
     }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+
+    /**
+     * @param string $first_name
+     * @return User
+     */
+    public function setFirstName($first_name)
+    {
+        $this->first_name = $first_name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    /**
+     * @param string $last_name
+     * @return User
+     */
+    public function setLastName($last_name)
+    {
+        $this->last_name = $last_name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
 }
